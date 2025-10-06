c1ee603154e35bba780f03cc4105d2022bd07488
Hp <Hp@Sanjay>
2025-10-06 10:46:07 +0530

    day 1 commited

---
+++ javaproject/src/day1/firstprogram.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello java World");
+	
+
+	}
+
+}
---
+++ javaproject/src/day1/identifiers.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class identifiers {
+
+	public static void main(String[] args) {
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="logesh"; //use camelcase
+		System.out.println("value is : "+ studentName);
+		
+
+	}
+
+}
