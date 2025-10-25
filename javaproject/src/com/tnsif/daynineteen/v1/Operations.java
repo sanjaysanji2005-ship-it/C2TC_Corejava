24f5df8fc81cb66847997c67cf9f46bcff3d2a2b
Hp <Hp@Sanjay>
2025-10-25 22:04:32 +0530

    commit

--- javaproject/src/com/tnsif/daynineteen/v1/Operations.java
+++
@@ -1,8 +0,0 @@
-//Define FunctionalInterface Operations
-
-package com.tnsif.daynineteen.v1;
-
-@FunctionalInterface
-public interface Operations {
-	float performArithmetic(int a, int b);
-}
--- javaproject/src/com/tnsif/daynineteen/v1/Palindrome.java
+++
@@ -1,7 +0,0 @@
-//Program to define generic functional interface Palindrome
-
-package com.tnsif.daynineteen.v1;
-
-public interface Palindrome<T> {
-	boolean checkPalindrome(T data);
-}
